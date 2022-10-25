20cb938fcfdf11f1c6aac83afd3fb5b9618cbe99
Leandro Paulo Bogoni <leandro.bogoni@procempa.com.br>
2022-10-25 15:41:32 -0300

    primeiro commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menu = () => {
+    return <>
+                <h1>Curso de React JS</h1>
+                <a href='#'>Departamentos</a> 
+                <a href='#'>Add Departamento</a>
+            </>
+}
+
+export default Menu;
